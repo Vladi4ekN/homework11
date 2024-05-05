1b8380b92f6425f9f91d1929cd5c32cb2a271a9c
Vladi4ekN <167472798+Vladi4ekN@users.noreply.github.com>
2024-05-05 14:42:45 +0300

    Add files via upload

---
+++ homework11.py
@@ -0,0 +1,15 @@
+def test():
+    a = 10
+    b = 20
+    print(a, b)
+
+
+test()
+def test2(men, women, children):
+    men = 10101
+    women = 11001
+    children = 10110
+    print(men, women, children)
+
+
+test2(1, 2, 3)
